84f32935b1044ed1df1a74247cca03e2d5e03754
itMeFiras <47608098+itMeFiras@users.noreply.github.com>
2019-02-17 20:18:13 +0100

    temp

--- src/baseBundle/Controller/DefaultController.php
+++ src/baseBundle/Controller/DefaultController.php
@@ -10,12 +10,54 @@ class DefaultController extends Controller
     {
         return $this->render('@base/Default/index.html.twig');
     }
+
+    public function aboutAction()
+    {
+        return $this->render('@base/Default/about.html.twig');
+    }
+
     public function blogAction()
     {
         return $this->render('@base/Default/blog.html.twig');
     }
-    public function aboutAction()
+
+    public function cartAction()
     {
-        return $this->render('@base/Default/about.html.twig');
+        return $this->render('@base/Default/cart.html.twig');
+    }
+
+    public function checkoutAction()
+    {
+        return $this->render('@base/Default/checkout.html.twig');
+    }
+
+    public function contactAction()
+    {
+        return $this->render('@base/Default/contact.html.twig');
+    }
+
+    public function portfolioAction()
+    {
+        return $this->render('@base/Default/portfolio.html.twig');
+    }
+
+    public function shopAction()
+    {
+        return $this->render('@base/Default/shop.html.twig');
+    }
+
+    public function shop_detailsAction()
+    {
+        return $this->render('@base/Default/shop-details.html.twig');
+    }
+
+    public function portfolio_detailsAction()
+    {
+        return $this->render('@base/Default/single-portfolio.html.twig');
+    }
+
+    public function blog_detailsAction()
+    {
+        return $this->render('@base/Default/single-post.html.twig');
     }
 }
