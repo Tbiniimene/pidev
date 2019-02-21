8af6b92d1cc293693635726e138a31b4f05e5074
medyacin <medyacin@users.noreply.github.com>
2019-02-21 03:28:17 +0100

    entity update

--- src/baseBundle/Entity/DetailCommande.php
+++ src/baseBundle/Entity/DetailCommande.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Entity;
+namespace baseBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
 
--- src/baseBundle/Entity/Livreur.php
+++ src/baseBundle/Entity/Livreur.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Entity;
+namespace baseBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
 
@@ -65,6 +65,118 @@ class Livreur
      * })
      */
     private $idDispo;
+
+    /**
+     * @return int
+     */
+    public function getIdLivreur()
+    {
+        return $this->idLivreur;
+    }
+
+    /**
+     * @param int $idLivreur
+     */
+    public function setIdLivreur($idLivreur)
+    {
+        $this->idLivreur = $idLivreur;
+    }
+
+    /**
+     * @return string
+     */
+    public function getNom()
+    {
+        return $this->nom;
+    }
+
+    /**
+     * @param string $nom
+     */
+    public function setNom($nom)
+    {
+        $this->nom = $nom;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPrenom()
+    {
+        return $this->prenom;
+    }
+
+    /**
+     * @param string $prenom
+     */
+    public function setPrenom($prenom)
+    {
+        $this->prenom = $prenom;
+    }
+
+    /**
+     * @return string
+     */
+    public function getEtat()
+    {
+        return $this->etat;
+    }
+
+    /**
+     * @param string $etat
+     */
+    public function setEtat($etat)
+    {
+        $this->etat = $etat;
+    }
+
+    /**
+     * @return int
+     */
+    public function getTel()
+    {
+        return $this->tel;
+    }
+
+    /**
+     * @param int $tel
+     */
+    public function setTel($tel)
+    {
+        $this->tel = $tel;
+    }
+
+    /**
+     * @return string
+     */
+    public function getLocalisation()
+    {
+        return $this->localisation;
+    }
+
+    /**
+     * @param string $localisation
+     */
+    public function setLocalisation($localisation)
+    {
+        $this->localisation = $localisation;
+    }
+
+    /**
+     * @return \Disponibilite
+     */
+    public function getIdDispo()
+    {
+        return $this->idDispo;
+    }
+
+    /**
+     * @param \Disponibilite $idDispo
+     */
+    public function setIdDispo($idDispo)
+    {
+        $this->idDispo = $idDispo;
+    }
 
 
 }
