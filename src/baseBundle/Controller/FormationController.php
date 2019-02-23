b12fda157fa231d946f8595e1a61d2a6b0985d07
imene <imene.tbini@esprit.tn>
2019-02-23 11:47:29 +0100

    formateur crud
    missing disponibilité

--- src/baseBundle/Controller/FormationController.php
+++ src/baseBundle/Controller/FormationController.php
@@ -35,4 +35,18 @@ class FormationController extends Controller
         return $this->redirectToRoute("formateur_afficherformateur");
     }
 
+    public function modifierAction(Request $request,$id) {
+        $formateur= $this->getDoctrine()->getRepository(Formateur::class)->find($id);
+        $form= $this->createForm(FormateurType::class,$formateur);
+        $form->handleRequest($request);
+        if ($form->isSubmitted()){
+            $em= $this->getDoctrine()->getManager();
+            $em->persist($formateur);
+            $em->flush();
+            return $this->redirectToRoute("formateur_afficherformateur");
+        }
+        return $this->render("@base/Formation/modif.html.twig",
+            array("form"=>$form->createView()));
+    }
+
 }
