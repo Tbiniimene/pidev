7a6da5df76a485743563f098f6d3b69a9f8aec10
medyacin <medyacin@users.noreply.github.com>
2019-02-27 01:30:19 +0100

    livreur front end

---
+++ src/livraisonBundle/Controller/livreurController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace livraisonBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class livreurController extends Controller
+{
+    public function loginAction()
+    {
+        if(isset($_POST['username']) && isset($_POST['pass']))
+        {
+            $name=$_POST['username'];
+            $pass=$_POST['pass'];
+
+            //check data base else render login route
+
+            return $this->redirectToRoute('livreur_index');
+        }
+
+        return $this->render('@livraison/livreur/login.html.twig');
+    }
+
+    public function indexAction()
+    {
+        return $this->render('@livraison/livreur/index.html.twig');
+
+    }
+
+
+}
